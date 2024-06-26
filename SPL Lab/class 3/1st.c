a3037d00c6339ce5bb707bbcfb6770db6a70ea1c
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-06-27 01:29:46 +0600

    solve and add lab class 3 comment is problem 1, 2

--- SPL Lab/class 3/1st.c
+++ SPL Lab/class 3/1st.c
@@ -1,18 +1,24 @@
+// if I Enter 7
+// print this 1, 0, 1, 0, 1, 0, 1
 #include<stdio.h>
 int main()
 {
-    int n, i;
-    scanf("%d",&n);
+    int n, i; // declare the variable
+    printf("Enter N: "); // for show you can a input
+    scanf("%d",&n);     // input store in variable
 
-    for(i=1; i<n; i++)
+    for(i=1; i<n; i++) // write a loop or run the program N times
     {
-        if(i%2==0)
+        if(i%2==0)  // define this is odd or even
             printf("0, ");
         else
             printf("1, ");
     }
+    // last write this part for I don't write last a comma",".
     if(i%2==0)
         printf("0 ");
     else
         printf("1 ");
+
+    return 0;
 }
--- SPL Lab/class 3/2nd.c
+++ SPL Lab/class 3/2nd.c
@@ -2,21 +2,23 @@
 #include<stdio.h>
 int main()
 {
-    int n, flag=1;
-    scanf("%d",&n);
-    int i;
-    for(i=2; i<n; i++)
+    int n, flag=1;                                          // input store in variable
+    printf("Enter Number you want to this is prime? : ");   // for show you can a input
+    scanf("%d",&n);                                         // input store in variable
+    int i;      // this line for loop initialised
+    for(i=2; i<n; i++) // write for loop
     {
-        if(n%i==0)
+        if(n%i==0) // we know the prime number n if I modulus by 2 to n-1 and find modulus, then I can told this id not prime number.
         {
-            flag=0;
-            break;
+            flag=0; // if n%i id true then change the flag variable 1 to 0
+            break;  // used break because if i can find modulus is 0 then we find this number is not prime number so we no need again run this programme so end the programme
         }
     }
-    if(n==1 || n==0){
+    if(n==1 || n==0)// again use if the user input 0 or 1 the 0 or 1 is not prime number because smell prime number is 2;
+    {
         printf("%d is not prime\n",n);
     }
-    else if(flag==1)printf("%d is prime\n",n);
+    else if(flag==1)printf("%d is prime\n",n); // in loop see this is prime or not if prime the flag variable can not change if change the programme shift else
 
     else printf("%d is not prime\n",n);
     return 0;
