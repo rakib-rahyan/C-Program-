5ab08efe7c5957d56a3eea6f2c8787307f2450e5
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-07-07 00:57:42 +0600

    solve problem 8 in spl lab preblem set in array

--- SPL Lab/Problem set and solve/05. Array/8th.c
+++ SPL Lab/Problem set and solve/05. Array/8th.c
@@ -3,6 +3,23 @@ minimum among them with its index position.*/
 #include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d")
+    int n, max,min,in,in2;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+        if(max<arr[i]){
+            max=arr[i];
+            in=i;
+        }
+    }
+    min=max;
+    for(int i=0; i<n; i++){
+        if(min>arr[i]){
+            min=arr[i];
+            in2=i;
+        }
+    }
+    printf("Max: %d, Index: %d\n",max, in);
+    printf("Min: %d, Index: %d",min, in2);
 }
