04ffad31b32a006585b84decf52815b5029ca13d
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-07-10 23:56:20 +0600

    solve practice book problem page 30 Q.viii

--- SPL Class/Solve Practice book problem/Page 30 Q.4 vii.c
+++ SPL Class/Solve Practice book problem/Page 30 Q.4 vii.c
@@ -5,10 +5,12 @@ int main()
     printf("Enter N integer number for show pattern: ");
     scanf("%d",&n);
     for(int i=0; i<n;i++){
-        for(int j=0; j<i; j++)printf(".");
-        for(int k=0; k<n-i; k++)printf("%c ",'A'+k);
+        for(int j=1; j<n-i; j++)printf(".");
+        for(int k=1; k<=i+1; k++)printf("%d",k);
+        for(int l=i; l>=1; l--)printf("%d",l);
         printf("\n");
         }
 }
+
 
 
