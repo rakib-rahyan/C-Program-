e40e83c2eebee74f75c2972bb6bd8c9e724957ff
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-09-09 12:10:15 +0600

    complet 1st example

---
+++ Apna college/Function/Example 1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+// Declaration/Prototype
+void printHello();
+void printName();
+int main()
+{
+    printHello(); //function call
+    printName(); //function call
+    return 0;
+}
+//function definition
+void printHello()
+{
+    printf("Hello!\n");
+}
+void printName()
+//function definition
+{
+    printf("I am Rakib Rahyan");
+}
