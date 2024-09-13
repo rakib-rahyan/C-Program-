bc9837d8d830eddce3fca14db7e356a63a7ce1ed
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-09-14 00:06:39 +0600

    complet 10th porblem in power calculate

--- SPL Lab/Problem set and solve/09. Function/10th.c
+++ SPL Lab/Problem set and solve/09. Function/10th.c
@@ -1,1 +1,21 @@
+#include<stdio.h>
+int CalPow(int n, int m);
+int main()
+{
+    int x, y;
+    scanf("%d%d",&x,&y);
+    printf("%d to the power %d is %d",x,y,CalPow(x,y));
+    return 0;
+}
+
+CalPow(int n, int m)
+{
+    int pow=1;
+    for(int i=0; i<m ;i++){
+        pow*=n;
+    }
+    return pow;
+}
+
+
 
