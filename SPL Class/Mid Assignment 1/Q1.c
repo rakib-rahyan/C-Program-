5631736b6a3fd917d9fbf79c5760b582808bc048
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-09-13 23:44:45 +0600

    mid assignment 1 re craction and in function 10th runing by comoplet

--- SPL Class/Mid Assignment 1/Q1.c
+++ SPL Class/Mid Assignment 1/Q1.c
@@ -7,20 +7,11 @@ int main()
     for(i=0; i<n; i++)
     {
         for( j=i; j<n-1; j++)printf("-");
-        for(int k=0; k<=i;)
+        for(int k=0; k<=i;k++)
         {
-            if(l%2==1)
-            {
-                printf("%d",l);
-            }
-            if(l%2==0)
-            {
-                k++;
-            }
-            l++;
-            if(l==5)
-            {
-                //printf("%d",l);
+            printf("%d",l);
+            l+=2;
+            if(l>n){
                 l=1;
             }
         }
