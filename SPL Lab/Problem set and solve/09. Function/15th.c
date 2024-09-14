97b9e21e42311716ca715817abe02149cb553b12
rakib-rahyan <rakibrahyan20232@gmail.com>
2024-09-14 20:30:24 +0600

    complete 15th function with maximun function

--- SPL Lab/Problem set and solve/09. Function/15th.c
+++ SPL Lab/Problem set and solve/09. Function/15th.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int MinimumValue(int n, int b[],int q);
+int MaxValu(int n, int b[],int q);
 int main()
 {
     int n,q;
@@ -13,6 +14,7 @@ int main()
         q=a[i];
     }
     printf("Minimum Value: %d",MinimumValue(n,a,q));
+    printf("\nMaximum Value: %d",MaxValu(n,a,q));
     return 0;
 }
 MinimumValue(int n, int b[],int q)
@@ -20,6 +22,18 @@ MinimumValue(int n, int b[],int q)
     for(int i=0; i<n; i++)
     {
         if(q>b[i])
+        {
+            q=b[i];
+        }
+    }
+    return q;
+}
+
+MaxValu(int n, int b[],int q)
+{
+    for(int i=0; i<n; i++)
+    {
+        if(q<b[i])
         {
             q=b[i];
         }
